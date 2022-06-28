658607863eb3b33b25d1ab9b816a157bc8f855f1
peter anyaogu <anyaogupeter601@gmail.com>
2022-06-28 23:03:25 +0100

    collapse ud-login module

--- frontend/components/Layout/Login/index.tsx
+++ frontend/components/Layout/Login/index.tsx
@@ -1,6 +1,38 @@
+import { useWeb3React } from "@web3-react/core";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import React, { FC } from "react";
+import connectors from "./connectors";
 
 const Connect: FC = () => {
+  const { active, account, activate, deactivate } = useWeb3React();
+
+  function createConnectHandler(connectorId: string) {
+    return async () => {
+      try {
+        const connector = connectors[connectorId];
+
+        // Taken from https://github.com/NoahZinsmeister/web3-react/issues/124#issuecomment-817631654
+        if (
+          connector instanceof WalletConnectConnector &&
+          connector.walletConnectProvider
+        ) {
+          connector.walletConnectProvider = undefined;
+        }
+
+        await activate(connector);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  }
+
+  async function handleDisconnect() {
+    try {
+      deactivate();
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
@@ -11,12 +43,28 @@ const Connect: FC = () => {
           </span>
         </a>
         <div className="flex md:order-2">
-          <button
-            type="button"
-            className="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800"
-          >
-            Login
-          </button>
+          {active ? (
+            <button
+              type="button"
+              className="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800"
+              onClick={handleDisconnect}
+            >
+              Disconnect {account}
+            </button>
+          ) : (
+            <div>
+              {Object.keys(connectors).map((v) => (
+                <button
+                  key={v}
+                  type="button"
+                  className="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800"
+                  onClick={createConnectHandler(v)}
+                >
+                  Connect to {v}
+                </button>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </nav>
