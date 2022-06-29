669aeef0958fb6dd429b61f4d6e6f26c86b9e443
peter anyaogu <anyaogupeter601@gmail.com>
2022-06-29 21:27:11 +0100

    clean up

--- frontend/components/Layout/Login/index.tsx
+++ frontend/components/Layout/Login/index.tsx
@@ -1,108 +1,90 @@
-import UAuth from '@uauth/js'
-import React, { useEffect, useState } from 'react'
+import UAuth from "@uauth/js";
+import React, { useEffect, useState } from "react";
 
 const uauth = new UAuth({
-
-
   clientID: "7f4f5811-e1a7-4787-a560-94dbdae94da3",
   redirectUri: "http://localhost:5000/callback",
-  scope: "openid wallet humanity_check"
-
-
-})
+  scope: "openid wallet humanity_check",
+});
 
 const App: React.FC = () => {
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState<Error>()
-  const [user, setUser] = useState<any>()
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error>();
+  const [user, setUser] = useState<any>();
 
   // Check to see if the user is inside the cache
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     uauth
       .user()
       .then(setUser)
       .catch(console.error)
-      .finally(() => setLoading(false))
-  }, [])
+      .finally(() => setLoading(false));
+  }, []);
 
   // Login with a popup and save the user
   const handleLogin = () => {
-    setLoading(true)
+    setLoading(true);
     uauth
       .loginWithPopup()
       .then(() => uauth.user().then(setUser))
       .catch(setError)
-      .finally(() => setLoading(false))
-  }
+      .finally(() => setLoading(false));
+  };
 
   // Logout and delete user
   const handleLogout = () => {
-    setLoading(true)
+    setLoading(true);
     uauth
       .logout()
       .then(() => setUser(undefined))
       .catch(setError)
-      .finally(() => setLoading(false))
-  }
+      .finally(() => setLoading(false));
+  };
 
   if (loading) {
-    return <>Loading...</>
+    return <>Loading...</>;
   }
 
   if (error) {
-    console.error(error)
-    return <>{String(error.stack)}</>
+    console.error(error);
+    return <>{String(error.stack)}</>;
   }
 
-  if (user) {
-    return (
-      <>
-
-        <pre>Connected to {user.wallet_address} {user.sub}</pre>
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-          <div className="container flex flex-wrap justify-between items-center mx-auto">
-            <a href="https://flowbite.com/" className="flex items-center">
-              {/*image*/}
-              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-                Dao
-              </span>
-            </a>
-            <div className="flex md:order-2">
+  return (
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <a href="https://localhost:3000/" className="flex items-center">
+          {/*image*/}
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            Dao
+          </span>
+        </a>
+        <div className="flex md:order-2">
+          {user ? (
+            <>
+              <div>
+                Connected to {user.wallet_address} {user.sub}
+              </div>
               <button
                 className="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800"
                 onClick={() => handleLogout()}
               >
                 Logout
               </button>
-            </div>
-          </div>
-        </nav>
-
-      </>
-    )
-  }
-
-  return (<nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-    <div className="container flex flex-wrap justify-between items-center mx-auto">
-      <a href="https://flowbite.com/" className="flex items-center">
-        {/*image*/}
-        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-          Dao
-        </span>
-      </a>
-      <div className="flex md:order-2">
-        <button
-          className="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800"
-          onClick={() => handleLogin()}
-        >
-          Login with Unstoppable
-        </button>
+            </>
+          ) : (
+            <button
+              className="text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800"
+              onClick={() => handleLogin()}
+            >
+              Login with Unstoppable
+            </button>
+          )}
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   );
+};
 
-}
-
-export default App
+export default App;
